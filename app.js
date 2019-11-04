d6bbcc9adc81b62019eaf2f580fca15d917385e0
FSDonnelly <donnelly_frank@yahoo.com>
2019-11-03 22:09:50 -0800

    created initial files structure, installed express, setup server routes in app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+let express = require('express');
+let app = express();
+const PORT = process.env.PORT || 3000;
+
+let products = [];
+let id = 1;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get(`/products`, (req, res) => {
+  res.send(products);
+});
+
+app.post(`/products`, (req, res) => {
+  let newProduct = req.body;
+  newProduct.id = id;
+  id++;
+  products.push(newProduct);
+  res.send(`Created`);
+});
+
+app.put(`/products/:id`, (req, res) => {
+  let productId = req.params.id;
+  let productForUpdate;
+  for (let i = 0; i < products.length; i++) {
+    let element = products[i];
+    if (element.id === productId) {
+      productForUpdate = element;
+      break;
+    }
+  }
+  if (productForUpdate) {
+    productForUpdate.price = req.body.price;
+    res.send(`Updated`);
+  } else {
+    res.send(`Rejected`);
+  }
+});
+
+app.delete(`/products/:id`, (req, res) => {
+  let productId = req.params.id;
+  for (let i = 0; i < products.length; i++) {
+    let element = products[i];
+    if (element.id === productId) {
+      products.splice(i, 1);
+      res.send(`Deleted`);
+    }
+  }
+  res.send(`Rejected`);
+});
+
+app.listen(PORT, () =>
+  console.log(`Server started on port http://localhost:${PORT}`)
+);
